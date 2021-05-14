d42a3967526002ac6668bece944713d9ad51a8e4
Nedas <vvinickas@gmail.com>
2021-05-15 02:42:07 +0300

    2 functions
    - Part list now is visible
    - Part balance can be updated

---
+++ Controllers/PartController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Autoservisas.Models;
+
+namespace Autoservisas.Controllers
+{
+    public class PartController : Controller
+    {
+        // GET: Part
+        public ActionResult Index()
+        {
+            Part db = new Part();
+            ModelState.Clear();
+            return View(db.GetPart());
+        }
+
+        // GET: Part/Edit/5
+        public ActionResult Edit(int id)
+        {
+            Part db = new Part();
+            return View(db.GetPart().Find(model => model.PartID == id));
+        }
+
+        // POST: Part/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, Part model)
+        {
+            try
+            {
+                Part db = new Part();
+                db.UpdateAmmount(model);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Models/Part.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Autoservisas.Models
+{
+    public class Part
+    {
+        [DisplayName("ID")]
+        public int PartID { get; set; }
+        [DisplayName("Kaina")]
+        [Required]
+        public double Price { get; set; }
+
+        [DisplayName("Pavadinimas")]
+        [Required]
+        public string Name { get; set; }
+
+        [DisplayName("Kodas")]
+        [Required]
+        public int Code { get; set; }
+
+        [DisplayName("Žymos")]
+        [Required]
+        public string Tags { get; set; }
+
+        [DisplayName("Nuotrauka")]
+        [Required]
+        public string Picture { get; set; }
+
+        [DisplayName("Aprašymas")]
+        [Required]
+        public string Details { get; set; }
+
+        [DisplayName("Likutis")]
+        [Required]
+        public int Ammount { get; set; }
+
+        [DisplayName("Kokybė")]
+        [Required]
+        public int Quality { get; set; }
+
+        [DisplayName("Kategorija")]
+        [Required]
+        public string Category { get; set; }
+
+        [DisplayName("Originalumas")]
+        [Required]
+        public bool Originallity { get; set; }
+
+        private SqlConnection con;
+        private void connection()
+        {
+            string constring = ConfigurationManager.ConnectionStrings["Database"].ToString();
+            con = new SqlConnection(constring);
+        }
+
+        public List<Part> GetPart()
+        {
+            connection();
+            List<Part> partlist = new List<Part>();
+
+            SqlCommand cmd = new SqlCommand("GetPartDetails", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            SqlDataAdapter sd = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+
+            con.Open();
+            sd.Fill(dt);
+            con.Close();
+
+            foreach (DataRow dr in dt.Rows)
+            {
+                partlist.Add(
+                    new Part
+                    {
+                        PartID = Convert.ToInt32(dr["id_simptomas"]),
+                        Price = Convert.ToDouble(dr["kaina"]),
+                        Name = Convert.ToString(dr["pavadinimas"]),
+                        Code = Convert.ToInt32(dr["detales_kodas"]),
+                        Tags = Convert.ToString(dr["zymos"]),
+                        Picture = Convert.ToString(dr["nuotrauka"]),
+                        Details = Convert.ToString(dr["aprasymas"]),
+                        Ammount = Convert.ToInt32(dr["likutis"]),
+                        Quality = Convert.ToInt32(dr["kokybe"]),
+                        Category = Convert.ToString(dr["kategorija"]),
+                        Originallity = Convert.ToBoolean(dr["originalumas"])
+                    });
+            }
+            return partlist;
+        }
+
+        public bool UpdateAmmount(Part model)
+        {
+            connection();
+            SqlCommand cmd = new SqlCommand("UpdatePartAmmount", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@id_detale", model.PartID);
+            cmd.Parameters.AddWithValue("@likutis", model.Ammount);
+
+            con.Open();
+            int i = cmd.ExecuteNonQuery();
+            con.Close();
+
+            if (i >= 1)
+                return true;
+            else
+                return false;
+        }
+    }
+}
